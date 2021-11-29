7d465530ab6d34b9326a863e59bcee54a7246c81
Globbox <khf168@khf.ru>
2021-11-29 16:01:50 +1000

    feat(CounterButtonApp): Основы Vue
    Реализовал функционал для подсчёта кликов по кнопке.

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,26 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({
+  name: 'Root',
+  data() {
+    return {
+      /**
+       * Количество кликов на кнопке.
+       */
+      buttonCounter: 0,
+    };
+  },
+
+  methods: {
+    /**
+     * Функция для инкментации значения переменной buttonCounter.
+     */
+    buttonCounterInc() {
+      this.buttonCounter++;
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+
+const vm = app.mount('#app');
